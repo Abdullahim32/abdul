f13af5659e8a3186ab72d1d550e4de8efa9c8bfb
Abdullahim32 <80367545+Abdullahim32@users.noreply.github.com>
2021-05-20 20:03:24 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# abdul
+# synchronousBallMovement
+Ball moving synchronously
---
+++ sketch.js
@@ -0,0 +1,94 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+
+var database;
+
+var form, player, game;
+
+
+function setup(){
+  canvas = createCanvas(400,400);
+ // database = firebase.database();
+  form = new Form()
+}
+
+
+function draw(){
+
+form.display()
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
